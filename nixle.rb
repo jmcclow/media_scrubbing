9e94944b8764a70c0a854fe714d4db7f8271d24e
Jay McClow <jay.mcclow@ge.com>
2016-06-14 04:51:18 -0700

    Adding in regex to sanitize data

--- nixle.rb
+++ nixle.rb
@@ -1,6 +1,8 @@
 #!/usr/local/bin/ruby
-require "net/https"
-require "uri"
+require 'rubygems'
+require 'bundler/setup'
+require 'net/https'
+require 'uri'
 require 'mechanize'
 
 http = Net::HTTP.new("nixle.com", 443)
@@ -14,6 +16,8 @@ agent = Mechanize.new { |agent|
 
 keep_movin = true
 on_page = 1
+regex_match = %r|(\d+)(?:\sblock\sof)(\s+\S+){3}|i
+regex_clean = %r|(block of)|i
 while keep_movin do
   agent.get("https://nixle.com/simi-valley-police-department/?page=#{on_page}")
   loop do
@@ -21,11 +25,19 @@ while keep_movin do
     pagina = agent.page
     next_link = agent.page.links_with(:text => 'More »').each do |link|
       link.click
-      puts agent.page.at('div[id="alert-body"]').text
+      page = agent.page.at('div[id="alert-body"]').text.match(regex_match)
+      if page
+        to_print = page.to_s.gsub(regex_clean, '')
+        puts to_print
+      end
     end
     advisory = agent.page.at('p.headline_agency')
     if advisory
-      puts advisory.text
+      page = advisory.text
+      if page
+        to_print = page.to_s.gsub(regex_clean, '')
+        puts to_print
+      end
     else
       break
     end
